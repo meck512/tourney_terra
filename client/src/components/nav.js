a3ce2abcc110184b38418d1b8db7d002411586ff
meck512 <m_eck12@outlook.com>
2021-11-27 14:33:29 -0500

    final file structure

---
+++ client/src/components/nav.js
@@ -0,0 +1,5 @@
+// // register button
+// login button
+// logout button
+
+// search button - search page - search form - search results page - search results list
---
+++ client/src/pages/UserProfile.js
@@ -0,0 +1,6 @@
+// profile info
+// update profile button - update profile page/modal - update profile form
+
+// my events list
+// post an event button - post event page/modal - post event form
+
