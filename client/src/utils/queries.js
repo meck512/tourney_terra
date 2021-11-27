53152f17149a1e008c0341baaa2a81be11635ece
twfb <twfb29@yahoo.com>
2021-11-27 18:29:15 -0500

    The utils/queries.js is set up

---
+++ client/src/utils/queries.js
@@ -0,0 +1,36 @@
+import gql from 'graphql-tag';
+
+export const QUERY_USER = gql`
+  query user($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      email
+    }
+  }
+`;
+
+export const QUERY_ME = gql`
+  {
+    me {
+      _id
+      username
+      email
+    }
+  }
+`;
+
+export const QUERY_ME_BASIC = gql`
+  {
+    me {
+      _id
+      username
+      email
+      friendCount
+      friends {
+        _id
+        username
+      }
+    }
+  }
+`;
