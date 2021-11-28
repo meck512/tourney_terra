b234d4f99570446f8a272518dde44b365e2e6295
meck512 <m_eck12@outlook.com>
2021-11-27 20:18:18 -0500

    apollo connect

--- server/server.js
+++ server/server.js
@@ -1,19 +1,40 @@
-const express = require("express");
+const express = require('express');
+// import ApolloServer
+const { ApolloServer } = require('apollo-server-express');
+
+// import our typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
+
+const PORT = process.env.PORT || 3001;
 const app = express();
-const cors = require("cors");
-require("dotenv").config({ path: "./config.env" });
-const port = process.env.PORT || 5000;
-app.use(cors());
-app.use(express.json());
-app.use(require("./routes/record"));
-// get driver connection
-const dbo = require("./db/conn");
 
-app.listen(port, () => {
-  // perform a database connection when server starts
-  dbo.connectToServer(function (err) {
-    if (err) console.error(err);
-
+const startServer = async () => {
+  // create a new Apollo server and pass in our schema data
+  const server = new ApolloServer({ 
+    typeDefs, 
+    resolvers, 
+    context: authMiddleware 
   });
-  console.log(`Server is running on port: ${port}`);
+
+  // Start the Apollo server
+  await server.start();
+
+  // integrate our Apollo server with the Express application as middleware
+  server.applyMiddleware({ app });
+
+  // log where we can go to test our GQL API
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+};
+
+// Initialize the Apollo server
+startServer();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
 });
